443b86434a7d4d0579a4e7881716c255fd9e05b0
Vijay Bhaskar Puttamshetty <vj.bhaskar012@gmail.com>
2019-06-27 13:19:48 +0200

    change db to postgres

--- gradingapp/settings.py
+++ gradingapp/settings.py
@@ -98,9 +98,18 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
