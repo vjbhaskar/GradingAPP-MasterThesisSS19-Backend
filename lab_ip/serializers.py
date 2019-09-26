84e3986e39564ce036c4a2bfbcaaef4e820cf2ae
Vijay Bhaskar Puttamshetty <vj.bhaskar012@gmail.com>
2019-09-26 11:04:36 +0200

    remove uned model

--- lab_ip/serializers.py
+++ lab_ip/serializers.py
@@ -22,13 +22,13 @@ class LabIpSerializer(serializers.ModelSerializer):
         # ip = self.initial_data['lab_id']
         # to get request args
         lab_id = self.initial_data['lab_id']
-        student_id = self.initial_data['student_id']
-        print('student_id ============', self.initial_data['student_id'])
+        # student_id = self.initial_data['student_id']
+        # print('student_id ============', self.initial_data['student_id'])
         print('lab id ============', self.initial_data['lab_id'])
-        student = User.objects.get(pk=student_id)
-        print(student)
+        # student = User.objects.get(pk=student_id)
+        # print(student)
         lab = Lab.objects.get(pk=lab_id)
-        lab_ip = LabIp(ip=ip, lab=lab, student=student)
+        lab_ip = LabIp(ip=ip, lab=lab)
         lab_ip.save()
         return lab_ip
 
